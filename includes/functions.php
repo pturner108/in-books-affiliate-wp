6226555952791fd9072c7b20160bb9a34cbd6912
rahilwazir <rahilwazir@hotmail.com>
2016-05-19 19:55:21 +0500

    Move taxonomy change to theme

--- includes/functions.php
+++ includes/functions.php
@@ -32,38 +32,3 @@ function iba_random_unique_id()
     }
     return implode($pass);
 }
-
-/**
- * Add main category to IBA Products
- */
-function iba_categories_for_products()
-{
-    register_taxonomy_for_object_type('category', 'product');
-}
-add_action('init', 'iba_categories_for_products');
-
-/**
- * Unregister IBA Product taxonomy
- */
-function iba_unregister_product_taxonomy()
-{
-    unregister_taxonomy('product_cat');
-}
-
-/**
- * Disabled the action causing woocommerce to throw error
- * Todo: Investigate
- */
-# add_action('woocommerce_after_register_taxonomy', 'iba_unregister_product_taxonomy');
-
-/**
- * Enable IBA Products with category search
- * @param $query WP_Query
- */
-function iba_products_on_category_pages($query)
-{
-    if ( $query->is_category() && $query->is_main_query() ) {
-        $query->set('post_type', array('post', 'product'));
-    }
-}
-add_action('pre_get_posts', 'iba_products_on_category_pages');
