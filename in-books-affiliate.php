ed35e7b928a030cdf1fc00881549e40063c726d6
Joshua Jacobs <josh@mandala-designs.com>
2017-03-03 12:10:02 -0500

    cleaned up xocbox availability message

--- in-books-affiliate.php
+++ in-books-affiliate.php
@@ -32,6 +32,7 @@ define('IBA\ASSETS_URI', trailingslashit(URI . 'assets'));
 require_once INC_DIR . 'class-related-list-table.php';
 require_once INC_DIR . 'class-metaboxes.php';
 require_once INC_DIR . 'class-shortcodes.php';
+require_once INC_DIR . 'class-xocbox-config.php';
 require_once INC_DIR . 'class-acf.php';
 require_once INC_DIR . 'class-post-types.php';
 require_once INC_DIR . 'functions.php';
@@ -71,6 +72,7 @@ final class Main {
 
         Metaboxes::init();
         Shortcodes::init();
+        XocboxConfig::init();
 
         register_activation_hook(__FILE__, array($this, 'activate'));
         register_deactivation_hook(__FILE__, array($this, 'deactivate'));
