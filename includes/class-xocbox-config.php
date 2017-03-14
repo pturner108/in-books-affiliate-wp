c606e4015f966418755c05017f91659b0a689821
Joshua Jacobs <josh@mandala-designs.com>
2017-03-14 04:45:27 -0400

    added filter for clearing the cart upon success

--- includes/class-xocbox-config.php
+++ includes/class-xocbox-config.php
@@ -9,8 +9,23 @@ namespace IBA;
 class XocboxConfig {
 
     public static function init() {
+        add_filter( 'woocommerce_payment_successful_result', array(__CLASS__, 'woocommerce_payment_successful_result'), 10, 2);
     }
 
+
+    /**
+     * The Xocbox payment method neglects to clear the cart after successful processing. This filter clears the cart.
+     *
+     * @param $result
+     * @param $order_id
+     * @return mixed
+     */
+    public static function woocommerce_payment_successful_result($result, $order_id) {
+        if ( isset( $result['result'] ) && 'success' === $result['result'] ) {
+            WC()->cart->empty_cart();
+        }
+        return $result;
+    }
 
 }
 
